5804c57008fb9a67316a829d98d28d2ce21430ec
Marius <marius@example.com>
2022-11-21 14:32:58 +0200

    2 images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+// #1 budas itraukti paveiksleli
+import house5 from './house5.jpg';
 
 function App() {
+  const name = 'James';
+
   return (
     <div className='App'>
-      <h1 className='mainTitle'>Hello react !!!</h1>
+      <h1 className='mainTitle'>
+        Hello111 {name} !!! {150 * 45}
+      </h1>
+      <h2>house 5</h2>
+      <img src={house5} alt='namas penktas' />
+      <hr />
+      <h2>House 2</h2>
+      <img src='/img/house2.jpg' alt='second house' />
     </div>
   );
 }
